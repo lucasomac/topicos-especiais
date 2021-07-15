5ff0a19ba554ec7d4085e4cbb800afb4ba69d4fb
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-07-14 22:11:25 -0300

    Added: first page

---
+++ src/pages/FirstPage/index.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import {Button, View, Text} from 'react-native';
+
+export default ({navigation}) => {
+    return (
+
+        <View style={{flex: 1, padding: 16, backgroundColor: "#FFC300"}}>
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                <Text
+                    style={{
+                        fontSize: 25,
+                        textAlign: 'center',
+                        marginBottom: 16,
+                    }}>
+                    This is the First Page under First Page Option
+                </Text>
+                <Button
+                    onPress={() => navigation.navigate('SecondPage')}
+                    title="Go to Second Page"
+                />
+                <Button
+                    onPress={() => navigation.navigate('ThirdPage')}
+                    title="Go to Third Page"
+                />
+            </View>
+            <Text style={{fontSize: 18, textAlign: 'center', color: 'grey'}}>
+                Custom React Navigate Drawer
+            </Text>
+            <Text style={{fontSize: 16, textAlign: 'center', color: 'grey'}}>
+                www.aboutreact.com
+            </Text>
+        </View>
+
+    );
+};
