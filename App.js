5822cd4a73d3f683cb098e91a32d5ae49629d0a5
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-07-14 22:12:31 -0300

    Added: app initial page

--- App.js
+++ App.js
@@ -1,21 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Custom Navigation Drawer / Sidebar with Image and Icon in Menu Options
+// https://aboutreact.com/custom-navigation-drawer-sidebar-with-image-and-icon-in-menu-options/
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+import {View, TouchableOpacity, Image} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {StatusBar} from 'expo-status-bar';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+import FirstPage from './src/pages/FirstPage';
+import SecondPage from './src/pages/SecondPage';
+import ThirdPage from './src/pages/ThirdPage';
+
+// Import Custom Sidebar
+import CustomSidebarMenu from './src/components/CustomSideBarMenu';
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const NavigationDrawerStructure = (props) => {
+    //Structure for the navigatin Drawer
+    const toggleDrawer = () => {
+        //Props to open/close the drawer
+        props.navigationProps.toggleDrawer();
+    };
+
+    return (
+        <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity onPress={toggleDrawer}>
+                {/*Donute Button Image */}
+                <Image
+                    source={{
+                        uri:
+                            'https://raw.githubusercontent.com/AboutReact/sampleresource/master/drawerWhite.png',
+                    }}
+                    style={{width: 25, height: 25, marginLeft: 5}}
+                />
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+function firstScreenStack({navigation}) {
+    return (
+        <Stack.Navigator initialRouteName="FirstPage">
+            <Stack.Screen
+                name="FirstPage"
+                component={FirstPage}
+                options={{
+                    title: 'First Page', //Set Header Title
+                    headerLeft: () => (
+                        <NavigationDrawerStructure navigationProps={navigation}/>
+                    ),
+                    headerStyle: {
+                        backgroundColor: '#E37D00', //Set Header color
+                    },
+                    headerTintColor: '#FFFFFF', //Set Header text color
+                    headerTitleStyle: {
+                        fontWeight: 'bold', //Set Header text style
+                    },
+                }}
+            />
+        </Stack.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function secondScreenStack({navigation}) {
+    return (
+        <Stack.Navigator
+            initialRouteName="SecondPage"
+            screenOptions={{
+                headerLeft: () => (
+                    <NavigationDrawerStructure navigationProps={navigation}/>
+                ),
+                headerStyle: {
+                    backgroundColor: '#E37D00', //Set Header color
+                },
+                headerTintColor: '#FFFFFF', //Set Header text color
+                headerTitleStyle: {
+                    fontWeight: 'bold', //Set Header text style
+                },
+            }}>
+            <Stack.Screen
+                name="SecondPage"
+                component={SecondPage}
+                options={{
+                    title: 'Second Page', //Set Header Title
+                }}
+            />
+            <Stack.Screen
+                name="ThirdPage"
+                component={ThirdPage}
+                options={{
+                    title: 'Third Page', //Set Header Title
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+function App() {
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <StatusBar style="light" backgroundColor="#AD6200"/>
+            <NavigationContainer>
+                <Drawer.Navigator
+                    drawerContentOptions={{
+                        activeTintColor: '#730000',
+                        itemStyle: {marginVertical: 5},
+                    }}
+                    drawerContent={(props) => <CustomSidebarMenu {...props} />}>
+                    <Drawer.Screen
+                        name="FirstPage"
+                        options={{drawerLabel: 'First screen'}}
+                        component={firstScreenStack}
+                    />
+                    <Drawer.Screen
+                        name="SecondPage"
+                        options={{drawerLabel: 'Second screen'}}
+                        component={secondScreenStack}
+                    />
+                </Drawer.Navigator>
+            </NavigationContainer>
+        </SafeAreaView>
+    );
+}
+
+export default App;
